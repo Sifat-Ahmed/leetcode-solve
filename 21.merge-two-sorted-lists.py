8af78d5c5c12e110ef669a5c070efa13f0522c16
Sifat Ahmed <sifat20210601@gmail.com>
2022-10-12 17:51:38 +0900

    solved 21

--- 21.merge-two-sorted-lists.py
+++ 21.merge-two-sorted-lists.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         ## creating two same nodes
